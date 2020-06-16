fe7f7f34b73cdc31f4d50f711e1f34a5287ad41f
kiyoday <1260583285@qq.com>
2020-06-16 22:26:09 +0800

    add mysql_connect test

--- html/index.php
+++ html/index.php
@@ -1,2 +1,15 @@
 <?php
+$dbms='mysql';     //数据库类型
+$host='db'; //数据库主机名
+$dbName='mysql';    //使用的数据库
+$user='root';      //数据库连接用户名
+$pass='root';          //对应的密码
+$dsn="$dbms:host=$host;dbname=$dbName";
+
+try {
+	$dbh = new PDO($dsn, $user, $pass);
+	echo '<h2>成功通过 PDO 连接到 MySQL 服务器</h2>' . PHP_EOL;
+} catch(PDOException $e) {
+	die("Error!: " . $e->getMessage() . "<br/>");
+}
 phpinfo();
